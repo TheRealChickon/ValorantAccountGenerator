038f4d563b4bb56578fe08cc8ddaf85433291928
TheRealChickon <x.samizerfaouix@gmail.com>
2023-10-19 13:02:30 +0200

    Update dependencies.bat

--- dependencies.bat
+++ dependencies.bat
@@ -2,3 +2,7 @@ pip install random_user_agent==1.0.1
 pip install selenium==4.2.0
 pip install webdriver_manager==3.5.4
 pip install undetected-chromedriver
+pip install discord-webhook
+pip install aiohttp
+pip install requests
+pip install fastapi
